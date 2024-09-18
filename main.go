f4f9b4422b5e0fdfdea5927c9c3d1518bed23c3c
KANGHO <ariaskkh@gmail.com>
2024-09-18 17:48:45 +0900

    go routine & channel

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 // func main() {
@@ -30,36 +29,55 @@ import (
 // 	fmt.Println(word)
 // }
 
-var errRequestFailed = errors.New("Request Failed")
+// var errRequestFailed = errors.New("Request Failed")
 
+// func main() {
+// 	var results = map[string] string {}
+// 	urls := [] string {
+// 		"https://www.airbnb.com/",
+// 		"https://www.naver.com/",
+// 		"https://www.google.com/",
+// 		"https://www.reddit.com/",
+// 		"https://www.facebook.com/",
+// 	}
+// 	for _, url := range urls {
+// 		result := "OK"
+// 		err := hitURL(url)
+// 		if err != nil {
+// 			result = "FAILED"
+// 		}
+// 		results[url] = result
+// 	}
+// 	for url, result := range results {
+// 		fmt.Println(url, result)
+// 	}
+// }
+
+// func hitURL(url string) error {
+// 	fmt.Println("Checking: ", url)
+// 	 resp, err := http.Get(url)
+// 	 if err != nil || resp.StatusCode >= 400 {
+// 		return errRequestFailed
+// 	 }
+// 	 return nil
+// }
+
+// goroutine. 프로그램이 작동하는 동안에만 유효
+// 메인함수가 goroutine을 기다려주지 않음
 func main() {
-	var results = map[string] string {}
-	urls := [] string {
-		"https://www.airbnb.com/",
-		"https://www.naver.com/",
-		"https://www.google.com/",
-		"https://www.reddit.com/",
-		"https://www.facebook.com/",
+	c := make(chan string)
+	people := [4]string {"dean", "aaaaa", "kango", "sellen"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	fmt.Println("Waiting for  messages")
+	// blocking operation
+	for i:=0; i < len(people); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
-	 resp, err := http.Get(url)
-	 if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
-	 }
-	 return nil
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + "is sexy"
 }
